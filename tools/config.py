cf25b6f1dc64e4027bc6adfb70a43c43652d400e
shezw <hello@shezw.com>
2024-11-23 22:46:38 +0800

    add more for config

--- tools/config.py
+++ tools/config.py
@@ -5,6 +5,11 @@
 import os
 import json
 import sys
+from rich.console import Console
+
+from tools.env import prepare_project
+
+console = Console()
 
 
 class CCConfig:
@@ -13,3 +18,62 @@ class CCConfig:
         self.configs = json.load(open(config_file_dir + '/../configs/ccroot.json'))
         self.lang = self.configs["lang"]
 
+
+def cc_root_init():
+    prepare_project(cc_root_get_project_dir())
+    # there are two main mode of the cc_root, source mode and library mode
+
+
+def cc_root_get_project_dir():
+    if os.getenv("CC_PROJ_ROOT_DIR") is not None:
+        return os.getenv("CC_PROJ_ROOT_DIR")
+    else:
+        return os.getcwd()
+
+
+def cc_root_get_project_cc_dir():
+    return cc_root_get_project_dir() + "/cc_root"
+
+
+def cc_root_get_project_config_file():
+    return cc_root_get_project_cc_dir() + "/cc_root_project.json"
+
+
+def cc_root_check_project():
+    # check if a .cc_root_project.json file exists
+    # in the current directory (shell running directory) or not
+    # if not, return False
+    # if exists, return True
+
+    # check if the .cc_root_project.json file exists in pwd directory
+    if os.path.exists(cc_root_get_project_config_file()):
+        return True
+    else:
+        return False
+
+
+def cc_root_config():
+    if not cc_root_check_project():
+        console.print("No project configuration file found in [ " + os.getcwd() + " ]")
+        console.print("Do you want to init as a new project? (y/n)")
+        # ask user if they want to init as a new project
+
+        user_input = input()
+        if user_input == "y":
+            cc_root_init()
+        elif user_input == "n":
+            print("No valid config found")
+            sys.exit(0)
+        else:
+            # ask again
+            cc_root_config()
+    else:
+        # load the project configuration file
+        console.print("Project configuration file found in [ " + os.getcwd() + " ]")
+        project = json.load(open(os.getcwd() + "/cc_root/cc_root_project.json"))
+        console.print(json.dumps(project, indent=4))
+
+
+def cc_root_get_config():
+    cc_config = CCConfig()
+    return cc_config.configs
--- tools/env.py
+++ tools/env.py
@@ -1,4 +1,5 @@
 # this file is used to set the environment of the cc_root
+import json
 import os
 
 
@@ -40,3 +41,59 @@ def prepare_env():
     for cc_root_directory in init_all_dirs:
         if not os.path.exists(cc_root_directory):
             os.makedirs(cc_root_directory)
+
+
+def prepare_project(project_dir):
+    CC_PROJ_ROOT_DIR = project_dir + "/cc_root"
+    CC_PROJ_HOST_DIR = CC_PROJ_ROOT_DIR + "/host"
+    CC_PROJ_HOST_BIN_DIR = CC_PROJ_HOST_DIR + "/bin"
+    CC_PROJ_HOST_LIB_DIR = CC_PROJ_HOST_DIR + "/lib"
+    CC_PROJ_HOST_INCLUDE_DIR = CC_PROJ_HOST_DIR + "/include"
+
+    CC_PROJ_BUILD_DIR = CC_PROJ_ROOT_DIR + "/build"
+    CC_PROJ_TARGET_DIR = CC_PROJ_ROOT_DIR + "/target"
+    CC_PROJ_OUT_DIR = CC_PROJ_ROOT_DIR + "/out"
+
+    # save all the project directories to the environment and cc_root_project.json
+    os.environ["CC_PROJ_ROOT_DIR"] = CC_PROJ_ROOT_DIR
+    os.environ["CC_PROJ_HOST_DIR"] = CC_PROJ_HOST_DIR
+    os.environ["CC_PROJ_HOST_BIN_DIR"] = CC_PROJ_HOST_BIN_DIR
+    os.environ["CC_PROJ_HOST_LIB_DIR"] = CC_PROJ_HOST_LIB_DIR
+    os.environ["CC_PROJ_HOST_INCLUDE_DIR"] = CC_PROJ_HOST_INCLUDE_DIR
+    os.environ["CC_PROJ_BUILD_DIR"] = CC_PROJ_BUILD_DIR
+    os.environ["CC_PROJ_TARGET_DIR"] = CC_PROJ_TARGET_DIR
+    os.environ["CC_PROJ_OUT_DIR"] = CC_PROJ_OUT_DIR
+
+    cc_root_project_config = {
+        "CC_PROJ_ROOT_DIR": CC_PROJ_ROOT_DIR,
+        "CC_PROJ_HOST_DIR": CC_PROJ_HOST_DIR,
+        "CC_PROJ_HOST_BIN_DIR": CC_PROJ_HOST_BIN_DIR,
+        "CC_PROJ_HOST_LIB_DIR": CC_PROJ_HOST_LIB_DIR,
+        "CC_PROJ_HOST_INCLUDE_DIR": CC_PROJ_HOST_INCLUDE_DIR,
+        "CC_PROJ_BUILD_DIR": CC_PROJ_BUILD_DIR,
+        "CC_PROJ_TARGET_DIR": CC_PROJ_TARGET_DIR,
+        "CC_PROJ_OUT_DIR": CC_PROJ_OUT_DIR
+    }
+
+    if not os.path.exists(CC_PROJ_ROOT_DIR):
+        os.makedirs(CC_PROJ_ROOT_DIR)
+    if not os.path.exists(CC_PROJ_HOST_DIR):
+        os.makedirs(CC_PROJ_HOST_DIR)
+    if not os.path.exists(CC_PROJ_HOST_BIN_DIR):
+        os.makedirs(CC_PROJ_HOST_BIN_DIR)
+    if not os.path.exists(CC_PROJ_HOST_LIB_DIR):
+        os.makedirs(CC_PROJ_HOST_LIB_DIR)
+    if not os.path.exists(CC_PROJ_HOST_INCLUDE_DIR):
+        os.makedirs(CC_PROJ_HOST_INCLUDE_DIR)
+    if not os.path.exists(CC_PROJ_BUILD_DIR):
+        os.makedirs(CC_PROJ_BUILD_DIR)
+    if not os.path.exists(CC_PROJ_TARGET_DIR):
+        os.makedirs(CC_PROJ_TARGET_DIR)
+    if not os.path.exists(CC_PROJ_OUT_DIR):
+        os.makedirs(CC_PROJ_OUT_DIR)
+
+    # save the project config to the .cc_root_project.json
+    with open(CC_PROJ_ROOT_DIR + "/cc_root_project.json", "w") as f:
+        f.write(json.dumps(cc_root_project_config, indent=4))
+
+    print("Project initialized successfully")
